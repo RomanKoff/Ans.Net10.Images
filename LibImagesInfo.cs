f12769cd994a4d787ac741a4e32a2b4dbd9850c1
Roman Koff <krv@guap.ru>
2026-01-22 21:34:13 +0300

    Add project files.

---
+++ LibImagesInfo.cs
@@ -0,0 +1,13 @@
+﻿using Ans.Net10.Common;
+
+namespace Ans.Net10.Images
+{
+
+	public static class LibImagesInfo
+	{
+		public static string GetName() => SuppApp.GetName();
+		public static string GetVersion() => SuppApp.GetVersion();
+		public static string GetDescription() => SuppApp.GetDescription();
+	}
+
+}
